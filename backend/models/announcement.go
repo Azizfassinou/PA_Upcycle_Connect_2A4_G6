da59a3a63280774741987076b45ed111523fb966
Azizfassinou <azizfassinou4@gmail.com>
2026-03-22 01:24:42 +0100

    Models User et announcement ajoutés

---
+++ backend/models/announcement.go
@@ -0,0 +1,15 @@
+
+type Announcement struct {
+	ID          uint           `json:"id" gorm:"primaryKey"`
+	Title       string         `json:"title" gorm:"not null"`
+	Description string         `json:"description"`
+	Category    string         `json:"category"`
+	Type        string         `json:"type"`
+	AuthorID    uint           `json:"author_id"`
+	Author      User           `json:"author" gorm:"foreignKey:AuthorID"`
+	Status      string         `json:"status" gorm:"default:'available'"`
+	ImageURL    string         `json:"image_url"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeleteAt    gorm.DeletedAt `json:"-" gorm:"index"`
+}
